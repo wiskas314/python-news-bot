f9134b7f4bbf48cd30e18a456a773f755d4f9722
wiskas314 <124578821+wiskas314@users.noreply.github.com>
2025-08-24 23:25:38 +0500

    Add files via upload

---
+++ topic.py
@@ -0,0 +1,94 @@
+CATEGORIES = {
+    "artificial intelligence": {
+        "url": "https://www.csoonline.com/artificial-intelligence/2/",
+        "eng": "artificial intelligence"
+    },
+    "generative ai": {
+        "url": "https://www.csoonline.com/generative-ai/2/",
+        "eng": "generative ai"
+    },
+    "business operations": {
+        "url": "https://www.csoonline.com/business-operations/2/",
+        "eng": "business operations"
+    },
+    "careers": {
+        "url": "https://www.csoonline.com/careers/2/",
+        "eng": "careers"
+    },
+    "industry": {
+        "url": "https://www.csoonline.com/industry/2/",
+        "eng": "industry"
+    },
+    "it leadership": {
+        "url": "https://www.csoonline.com/it-leadership/2/",
+        "eng": "it leadership"
+    },
+    "compliance": {
+        "url": "https://www.csoonline.com/compliance/2/",
+        "eng": "compliance"
+    },
+    "security": {
+        "url": "https://www.csoonline.com/security/2/",
+        "eng": "security"
+    },
+    "application security": {
+        "url": "https://www.csoonline.com/application-security/2/",
+        "eng": "application security"
+    },
+    "business continuity": {
+        "url": "https://www.csoonline.com/business-continuity/2/",
+        "eng": "business continuity"
+    },
+    "cloud security": {
+        "url": "https://www.csoonline.com/cloud-security/2/",
+        "eng": "cloud security"
+    },
+    "critical infrastructure": {
+        "url": "https://www.csoonline.com/critical-infrastructure/2/",
+        "eng": "critical infrastructure"
+    },
+    "cybercrime": {
+        "url": "https://www.csoonline.com/cybercrime/2/",
+        "eng": "cybercrime"
+    },
+    "identity and access management": {
+        "url": "https://www.csoonline.com/identity-access-management/2/",
+        "eng": "identity and access management"
+    },
+    "it management": {
+        "url": "https://www.csoonline.com/it-management/2/",
+        "eng": "it management"
+    },
+    "network security": {
+        "url": "https://www.csoonline.com/network-security/2/",
+        "eng": "network security"
+    },
+    "physical security": {
+        "url": "https://www.csoonline.com/physical-security/2/",
+        "eng": "physical security"
+    },
+    "privacy": {
+        "url": "https://www.csoonline.com/privacy/2/",
+        "eng": "privacy"
+    },
+    "risk management": {
+        "url": "https://www.csoonline.com/risk-management/2/",
+        "eng": "risk management"
+    },
+    "security infrastructure": {
+        "url": "https://www.csoonline.com/security-infrastructure/2/",
+        "eng": "security infrastructure"
+    },
+    "vulnerabilities": {
+        "url": "https://www.csoonline.com/vulnerabilities/2/",
+        "eng": "vulnerabilities"
+    },
+    "analytics": {
+        "url": "https://www.csoonline.com/analytics/2/",
+        "eng": "analytics"
+    },
+    "software development": {
+        "url": "https://www.csoonline.com/software-development/2/",
+        "eng": "software development"
+    }
+}
